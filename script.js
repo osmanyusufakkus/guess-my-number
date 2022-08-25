ccb81497c99a8ac194414c7397350199fcc795df
osmanyusufakkus <osmanyusufakkus@gmail.com>
2022-08-25 22:35:47 +0300

    Create random integer creator function
    - text language changed to englısh
    - message functıon used

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 'use strict';
-let deger = Math.floor(Math.random() * 21);
-console.log(deger);
 
+const randomNumbers = function (start, end) {
+  return Math.floor(Math.random() * end) + start;
+};
+const deger = randomNumbers(1, 20);
+console.log(deger);
 let score = document.querySelector('.score').textContent;
 let yuksekskor = 0;
+
+const mesajFonk = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.again').addEventListener('click', function () {
   deger = Math.floor(Math.random() * 21);
   console.log(deger);
   score = 20;
   document.querySelector('.score').textContent = 20;
-  document.querySelector('.message').textContent = 'Tahmine başla...';
+  mesajFonk('Start guessing...');
   document.querySelector('#kutu').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
@@ -21,32 +28,28 @@ document.querySelector('.check').addEventListener('click', function () {
   if (score > 1) {
     if (tahmin <= 20 && tahmin >= 0) {
       if (tahmin == deger) {
-        document.querySelector('.message').textContent = 'Helal lan';
+        mesajFonk('Congratulations!');
         document.querySelector('#kutu').textContent = deger;
         document.querySelector('body').style.backgroundColor = '#60b347';
         if (score > yuksekskor) {
           yuksekskor = score;
           document.querySelector('.highscore').textContent = yuksekskor;
         }
-      } else if (tahmin < deger) {
-        document.querySelector('.message').textContent = 'Düşük';
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else if (tahmin > deger) {
-        document.querySelector('.message').textContent = 'Yüksek';
+      } else if (tahmin !== deger) {
+        mesajFonk(tahmin < deger ? 'Too low' : 'Too high');
         score--;
         document.querySelector('.score').textContent = score;
       }
     } else {
-      document.querySelector('.message').textContent = 'La düzgün sayı gir';
+      mesajFonk('Between 1 and 20!!!');
       document.querySelector('.score').textContent = score;
     }
   } else {
-    document.querySelector('.message').textContent = 'Hakkın bitti. Kaybettin';
+    mesajFonk('You Lost');
     document.querySelector('.score').textContent = 0;
   }
 
   if (!tahmin) {
-    document.querySelector('.message').textContent = 'Bir sayı yaz...';
+    mesajFonk('Bir sayı yaz...');
   }
 });
